b244debd30e3f7977f9bbe2f47cbf48ea6b4e323
LS-AltControl <a7exlh@pm.me>
2025-03-22 13:46:25 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,25 +1,20 @@
-_G.LSDropper = {
-    -- Controllers: Enter the user IDs (or names) allowed to control the bots
-    Controllers = {
-        "7451107665",
-    },
-    
-    -- alts: Put all alt user IDs here
-    alts = {
-        3034352629,
-        7178503675,
-        7707204045,
-        7707164601,
-        7707251668,
-        7707159035,
-        7707264889,
-        8055473927,
-        7707209002,
-    },
-    
-    -- Prefix: Enter the prefix before each command.
-    Prefix = "/"
-}
+script_key = 'joIBLGqlUfbonOcQoYjMtYfclJUbDVVQ'
+getgenv().AutofarmSettings = {
+    ["Fps"] = 10,
+    ["InstaTP"] = true,
+    ["Underground"] = true,
 
--- Loader: Loads the main script logic
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LS-AltControl/LS-Control/refs/heads/main/Loader.lua"))()
+    ["Webhook"] = {
+        ["URL"] = "https://discord.com/api/webhooks/1349392866446807050/GGyeqFg5e8JxllAJWpyxFRAT4DZWxMQVTmDMg9cCLFctrJDSQ-hp5lqIzPOVANpT2lTF",
+        ["Interval"] = 10
+    },
+
+    ["Serverhop"] = {
+        ["Cycle"]   = 25,        -- 1 = After dying once.
+        ["Time"]    = 0,        -- 1 = After 1 Minute.
+        ["Kick"]    = false,    -- true = After getting kicked.
+        ["Blacklisted_IDs"] = { 7707159035, 7707263046, 7707251668, 8055468531, 2939174150, 7707209002, 7178503675, 8055473927, 2827160867, 8055446371, 7707261230, 228432957, 3034352629, 7707264889, 7707164601, 7707204045, 8195210, 439942262, 93101606, 163721789, 3944434729, 4255947062, 1830168970, 29242182 } -- If UserID was found ingame, Detects new joining players too.
+    },
+    " warlocks atm farm - @snuffing "
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2f5a5d4b9fc7ed0f115580a53bfab777.lua"))()
